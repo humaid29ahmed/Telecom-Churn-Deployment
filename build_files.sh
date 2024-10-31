5e6c7e34f41e48eea97802145f4a4c6db22f7c16
Humaid Ahmed <68558140+humaid29ahmed@users.noreply.github.com>
2024-10-31 23:42:56 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
-# build_files.sh
-pip install -r requirements.txt
+#!/bin/bash
 
-# make migrations
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py migrate 
+# Use python3 to install dependencies
+python3 -m pip install -r requirements.txt
+
+# Run Django management commands
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
